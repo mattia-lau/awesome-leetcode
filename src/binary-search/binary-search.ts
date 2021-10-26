1fe6df9642d83c58af43ea118a3ffeb2f3fa03f6
Mattia <siuwingabc1997@gmail.com>
2021-10-26 22:00:25 +0800

    feat: binary search

---
+++ src/binary-search/binary-search.ts
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/binary-search/
+
+const binarySearch = (
+  nums: number[],
+  target: number,
+  l: number,
+  r: number,
+): number => {
+  if (l > r) return -1;
+
+  const mid = Math.floor((l + r) / 2);
+
+  if (nums[mid] > target) {
+    return binarySearch(nums, target, l, mid - 1);
+  }
+
+  if (nums[mid] < target) {
+    return binarySearch(nums, target, mid + 1, r);
+  }
+
+  return mid;
+};
+
+export const search = (nums: number[], target: number): number => binarySearch(nums, target, 0, nums.length - 1);
