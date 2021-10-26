a267d12b3a60010b04949d2fc395bc9e6cf7dd23
Mattia <siuwingabc1997@gmail.com>
2021-10-26 20:13:09 +0800

    feat: first bad version

---
+++ src/first-bad-version/first-bad-version.test.ts
@@ -0,0 +1,25 @@
+import { solution } from '@awesome-leetcode/first-bad-version/binary-search';
+
+const testcases = [
+  {
+    n: 5,
+    bad: 4,
+    answer: 4,
+  },
+  {
+    n: 1,
+    bad: 1,
+    answer: 1,
+  },
+];
+
+describe('First Bad Version', () => {
+  describe('Binary search', () => {
+    test.each(testcases)('%s', ({ n: question, bad, answer }) => {
+      const isBadVersion = (n: number) => n >= bad;
+      const result = solution(question, isBadVersion);
+
+      expect(result).toEqual(answer);
+    });
+  });
+});
---
+++ src/first-bad-version/first-bad-version.ts
@@ -0,0 +1,13 @@
+type IsBadVersion = (n: number) => boolean;
+
+export const binarySearch = (l: number, r: number, isBadVersion: IsBadVersion) => {
+  const mid = Math.floor((l + r) / 2);
+  if (l > r) return l;
+
+  if (isBadVersion(mid)) {
+    return binarySearch(l, mid - 1, isBadVersion);
+  }
+  return binarySearch(mid + 1, r, isBadVersion);
+};
+
+export const solution = (n: number, isBadVersion: IsBadVersion) => binarySearch(0, n, isBadVersion);
